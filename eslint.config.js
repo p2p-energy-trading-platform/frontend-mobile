a2701657684abf31bde673118cbcb1fd5a67c5ce
Keeththigan <keeththi2003@gmail.com>
2026-07-11 21:52:06 +0530

    feat: update ESLint configuration and dependencies for improved linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,39 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const tseslint = require("typescript-eslint");
 
 module.exports = defineConfig([
   expoConfig,
+  ...tseslint.configs.recommended,
   {
-    ignores: ['dist/*'],
+    
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-debugger": "error",
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "import/order": [
+        "warn",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+    },
+
+    ignores: [
+      "dist",
+      "node_modules",
+      ".expo",
+    ],
   },
 ]);
