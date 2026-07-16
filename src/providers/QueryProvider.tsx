444d060887b44ccc18be6a257a2c605a92e3a0ad
Keeththigan <keeththi2003@gmail.com>
2026-07-16 14:54:20 +0530

    feat: add QueryProvider component for React Query integration

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from '@/lib';
+
+export function QueryProvider({
+  children,
+}: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
